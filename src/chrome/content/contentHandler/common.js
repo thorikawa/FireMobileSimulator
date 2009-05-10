cdd411234a6b1ddafb82018d0cf40462989c96b3
thorikawa <thorikawa@d0d07461-0603-4401-acd4-de1884942a52>
2009-05-10 05:13:17 +0000

    fix #160
    git-svn-id: http://svn.coderepos.org/share/platform/firefox/FireMobileSimulator/branches/1.1.X@33211 d0d07461-0603-4401-acd4-de1884942a52

--- src/chrome/content/contentHandler/common.js
+++ src/chrome/content/contentHandler/common.js
@@ -1,21 +1,22 @@
-/* ***** BEGIN LICENSE BLOCK Version: GPL 3.0 ***** 
- * FireMobileFimulator is a Firefox add-on that simulate web browsers of 
- * japanese mobile phones.
- * Copyright (C) 2008  Takahiro Horikawa <horikawa.takahiro@gmail.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * ***** END LICENSE BLOCK ***** */
+/*******************************************************************************
+ * ***** BEGIN LICENSE BLOCK Version: GPL 3.0 FireMobileFimulator is a Firefox
+ * add-on that simulate web browsers of japanese mobile phones. Copyright (C)
+ * 2008 Takahiro Horikawa <horikawa.takahiro@gmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>. ***** END LICENSE
+ * BLOCK *****
+ */
 
 var firemobilesimulator;
 if (!firemobilesimulator)
@@ -26,10 +27,13 @@ if (!firemobilesimulator.contentHandler)
 firemobilesimulator.contentHandler.common = {
 	filter : function (ndDocument, deviceId) {
 		if (ndDocument.body) {
-			//フォントを等幅に統一
+			// フォントを等幅に統一
 			ndDocument.body.style.fontFamily = "monospace";
+			
+			// 禁則処理を追加
+			firemobilesimulator.contentHandler.common.recursiveParse(ndDocument.body);
 	
-			//表示領域サイズの制御（現在は横幅のみ）
+			// 表示領域サイズの制御（現在は横幅のみ）
 			var forceScreenWidth = firemobilesimulator.common.pref
 					.getBoolPref("msim.config.general.force-screen-width");
 			var forceScreenHeight = firemobilesimulator.common.pref
@@ -77,5 +81,29 @@ firemobilesimulator.contentHandler.common = {
 				}
 			}
 		};
+	},
+	
+	recursiveParse : function (pNode) {
+		var childs = pNode.childNodes;
+		dump("parseChilds:"+childs.length+"\n");
+		for (var i = 0; i < childs.length; i++) {
+			var cNode = childs[i];
+			if (childs[i].nodeType == 1) {
+				firemobilesimulator.contentHandler.common.recursiveParse(childs[i]);
+			}else if(cNode.nodeType == 3) {
+				if(cNode.nodeValue.match("[^\n ]")){
+					var separator;
+					if(navigator.userAgent.indexOf('Firefox/2') != -1) {
+						separator = '<wbr />'
+					} else {
+						separator = String.fromCharCode(8203);
+					}
+					dump("word-break:replace textNode\n")
+					var chNode = document.createTextNode(cNode.nodeValue.split("").join(separator));
+					dump(cNode.nodeValue.split("").join(separator) + "\n");
+					cNode.parentNode.replaceChild(chNode,cNode);
+				}
+			}
+		}
 	}
 };
